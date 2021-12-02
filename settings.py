500eba23ce040b7d8bfd1c3190a7d4048b57977e
MonsterGaming375 <84940458+MonsterGaming375@users.noreply.github.com>
2021-12-02 14:43:38 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,17 @@
+# X is a rectangle and the P is your player, change the map to what you want, and place the P where you want your player to spawn
+level_map = [
+'                            ',
+'                            ',
+'        P                   ',
+' XX    XXX            XX    ',
+' XX                         ',
+' XXXX         XX         XX ',
+' XXXX    XXXXX              ',
+' XX    XXXXXXX    XX  XX    ',
+'       XXXXXXX    XX  XXX   ',
+'    XXXXXXXXXXXX  XX  XXXX  ',
+'XXXXXXXXXXXXXXXX  XX  XXXX  ']
 
+tile_size = 64
+screen_width = 1600
+screen_height = len(level_map) * tile_size
